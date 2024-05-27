f265ed9dabbc75836ef8a27c6edd113baf20674e
arsalankazmi7 <arsalankazmi7@gmail.com>
2024-05-27 22:57:37 +0530

    bug fix

--- Arrays/avgArray.c
+++ Arrays/avgArray.c
@@ -10,25 +10,25 @@ int main()
   printf("Enter the size of array : ");
   scanf("%d", &size);
 
-  int arr[size];
+  float arr[size];
 
   printf("Enter array elements : ");
   for (int i = 0; i < size; i++)
   {
-    scanf("%d", &arr[i]);
+    scanf("%f", &arr[i]);
     sum += arr[i];
   }
 
   printf("Array elements are : ");
   for (int i = 0; i < size; i++)
   {
-    printf("%d ", arr[i]);
+    printf("%0.2f ", arr[i]);
   }
 
   printf("\n");
 
   avg = sum / size;
-  printf("Average : %f", avg);
+  printf("Average : %0.2f", avg);
 
   return 0;
 }
